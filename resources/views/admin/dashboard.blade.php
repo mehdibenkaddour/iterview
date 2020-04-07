2cfdf5efdf34ae753d6616f51143d318c01058fc
HAMDAOUI Mohammed Yassin <medyassinham@gmail.com>
2020-04-07 16:26:38 +0100

    Adding Procfile for heroku

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,15 +6,15 @@ Admin-Panel
 
 @section('content')
 <div class="row">
-          <div class="col-md-12">
-            <div class="card">
-              <div class="card-header">
-                <h4 class="card-title">Welcome to your dashboard !</h4>
-              </div>
-              <div class="card-body">
-              </div>
-            </div>
-          </div>
+<div class="col-md-12">
+  <div class="card">
+    <div class="card-header">
+      <h4 class="card-title">Welcome to your dashboard !</h4>
+    </div>
+    <div class="card-body">
+    </div>
+  </div>
+</div>
 @stop
 
 @section('scripts')
