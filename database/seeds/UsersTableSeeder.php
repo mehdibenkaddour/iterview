d353401611278b7b76854d54482c6ba24afcfcce
HAMDAOUI Mohammed Yassin <medyassinham@gmail.com>
2020-04-07 23:47:24 +0100

    Adding seeder for default user

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name'     => 'admin name',
+            'email'    => 'iterview@admin.com',
+            'role'     => 'admin',
+            'password' => Hash::make('password')
+        ]);
+    }
+}
